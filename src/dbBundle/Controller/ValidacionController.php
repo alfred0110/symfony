e5d7ff74874c488f2d94aefad60273451513e914
Alfred <alf.nava.mend@gmail.com>
2018-02-22 19:04:38 -0600

    Validacion Formulario

--- src/dbBundle/Controller/ValidacionController.php
+++ src/dbBundle/Controller/ValidacionController.php
@@ -4,10 +4,11 @@ namespace dbBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use dbBundle\Entity\Autor;
 
 /**
- * @Route("/validar")
+ * @Route("/validate")
  */
  class ValidacionController extends Controller
  {
@@ -18,15 +19,40 @@ use dbBundle\Entity\Autor;
     {
       $autor = new Autor();
       $autor->setName("");
-      
+
       $validator = $this->get('validator');
       $errors = $validator->validate($autor);
 
       if(count($errors) > 0){
-        return new Response (print_r($errors,true));
+        //return new Response (print_r($errors,true));
+        return $this-> render('@db/Autor/validate.html.twig', array('errors' => $errors , ));
+
       }else{
         return new Response('The autor is valid');
       }
+    }
+
+    /**
+     * @Route("/update")
+     */
+    public function updateAction(Request $request)
+    {
+      $autor = new Autor();
+      $autor->setName("Alf");
+
+      $form = $this->createFormBuilder($autor)
+        ->add('name')
+        ->getForm();
+
+        if($form->isvalid())
+        {
+          return $this->render('@db/Autor/form.html.twig', array(
+            'form' => $form->createView(),
+          ));
+        }
+        else {
+              return new Response('The form is not valid');
+        }
     }
 }
  ?>
